80b5b1c56ae0db2bdcc408e44c6444247a2aee69
Kory-ifcd0210 <korypf@gmail.com>
2024-08-19 19:05:28 +0100

    tutorial section template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { OnboardComponent } from './onboard/onboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', component: OnboardComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'tutorial', component: TutorialComponent},
     { path: '**', component: PageNotFoundComponent },
-
 ];
--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatStepperModule} from '@angular/material/stepper';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-tutorial',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatStepperModule, RouterLink, MatIconModule],
   templateUrl: './tutorial.component.html',
-  styleUrl: './tutorial.component.css'
+  styleUrl: './tutorial.component.scss'
 })
 export class TutorialComponent {
 
